42c629d2f2f436b184c1cc1ce06bacb818be1604
Cristopher Ferrari <ferraricristopher93@gmail.com>
2023-11-27 21:03:53 +0100

    Consegna bonus

--- js/script.js
+++ js/script.js
@@ -14,39 +14,41 @@ do la possibilità all’utente di inserire voci nella lista
 // Definisco una lista della spesa vuota che l'utente dovra riempire finche continua ad inserire oggetti nel riqadro, per inserire gli oggetti nella lista, dovra cliccare sul tasto + , il tasto piu attivera una funzione che dovra far partire un loop fino ad un massimo di 50 elementi per lista della spesa;
 
 //step 1 - definire la lista della spesa e il bottone
+let spesa = [];
 
+//funzione che permette all'utente di aggiungere elementi alla lista
 
-let plusButton = document.getElementById('plus');
-// step 2 attivare l'evento click
-
-plusButton.addEventListener('click', function(){
-
-    
-    listaUl = document.querySelector('.Ullist');
-
-    
-    let listaSpesa = ['pane', 'pasta', 'mele', 'pere', 'maiale', 'branzino'];
-
-    
-    let elementList = '';
-
-   
-
-    let i = 0;
-
-    while (i < listaSpesa.length) {
-
-     
-
-      
-        elementList = `<li> ${listaSpesa[i]} </li>`
-
-   
-        listaUl.innerHTML += elementList;
-
-        i++;
-
+function aggiungiElemento() {
+    //prendo l'input dall'utente
+    let elementoInput = document.getElementById('elemento-input');
+    // trasformo l'input in una variabile
+    let elemento = elementoInput.value;
+    // condizione (se la casella ha del testo, l'iterazione verrà aggiunta alla lista della spesa, parte la funzione che aggiornerà la lista)
+    if(elemento.trim() !== '') {
+        spesa.push(elemento);
+        //parte la funzione che aggiornerà la lista
+        aggiornaLista();
+        //aggiungo il testo della casella 
+        elementoInput.value = '';
     }
 }
-);
-
+// funzione che permette di aggiornare la lista della spesa
+function aggiornaLista() {
+    //definisco variabile che prende la lista della spesa (UL)
+    let listaElementi = document.getElementById('spesa-lista');
+    //definisco alla variabile il valore stringa;
+    listaElementi.innerHTML = '';
+    //variabile i = 0
+    let i = 0;
+    //loop che varra finche si inserisce del testo nella casella;
+    while (i < spesa.length) {
+        //creo un li
+        let listItem = document.createElement('li');
+        //aggiungo al li un nodo testuale che vale come iterazione aggiunta dal testo del cinput text
+        listItem.append(document.createTextNode(spesa[i]));
+        //appendo alla unordered list il suo list item
+        listaElementi.append(listItem);
+        //aumento l'iterazione alla pressione del bottone '+'
+        i++;
+    }
+}
